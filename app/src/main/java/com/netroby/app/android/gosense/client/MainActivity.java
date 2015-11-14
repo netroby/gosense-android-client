e60e89cd976ae945ec33e231047d14bc27e68c9c
ZhiFeng Hu <hufeng1987@gmail.com>
2015-11-14 16:18:09 +0800

    Try to read jsonarray
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- app/src/main/java/com/netroby/app/android/gosense/client/MainActivity.java
+++ app/src/main/java/com/netroby/app/android/gosense/client/MainActivity.java
@@ -7,18 +7,25 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.JsonReader;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -45,28 +52,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+    private class DownloadWebpageTask extends AsyncTask<String, Void, JSONArray> {
         @Override
-        protected String doInBackground(String... urls) {
+        protected JSONArray doInBackground(String... urls) {
 
             // params comes from the execute() call: params[0] is the url.
             try {
                 return downloadUrl(urls[0]);
             } catch (IOException e) {
-                return "Unable to retrieve web page. URL may be invalid.";
+                Log.d(TAG, "doInBackground: " + e);
+                return new JSONArray();
             }
         }
         // onPostExecute displays the results of the AsyncTask.
         @Override
-        protected void onPostExecute(String result) {
+        protected void onPostExecute(JSONArray result) {
             TextView tv = (TextView) findViewById(R.id.textView);
-            tv.setText(result);
+            try {
+                tv.setText(result.get(0).toString());
+            } catch (Exception e) {
+                Log.d(TAG, "onPostExecute: " + e);
+            }
         }
-        private String downloadUrl(String myurl) throws IOException {
+        private JSONArray downloadUrl(String myurl) throws IOException {
             InputStream is = null;
             // Only display the first 500 characters of the retrieved
             // web page content.
-            int len = 500;
 
             try {
                 URL url = new URL(myurl);
@@ -82,21 +93,30 @@ public class MainActivity extends AppCompatActivity {
                 is = conn.getInputStream();
 
                 // Convert the InputStream into a string
-                return readIt(is, len);
+                String respString = readIt(is);
+
+                return new JSONArray(respString);
 
                 // Makes sure that the InputStream is closed after the app is
                 // finished using it.
+            } catch (Exception e) {
+                Log.d(TAG, "downloadUrl: " + e);
             } finally {
                 if (is != null) {
                     is.close();
                 }
             }
+            return new JSONArray();
         }
-        public String readIt(InputStream stream, int len) throws IOException {
-            Reader reader = new InputStreamReader(stream, "UTF-8");
-            char[] buffer = new char[len];
-            reader.read(buffer);
-            return new String(buffer);
+        public String readIt(InputStream stream) throws IOException {
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+            return sb.toString();
         }
     }
 
