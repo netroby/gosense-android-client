3dfd445e090c7f1ac6717407bf828aa51a259d4e
ZhiFeng Hu <hufeng1987@gmail.com>
2015-11-19 13:48:15 +0800

    Add toolbar
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- app/src/main/java/com/netroby/app/android/gosense/client/MainActivity.java
+++ app/src/main/java/com/netroby/app/android/gosense/client/MainActivity.java
@@ -4,11 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.JsonReader;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,11 +23,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -40,10 +35,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new DownloadWebpageTask().execute("https://www.netroby.com/api");
-        /**
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        new DownloadWebpageTask().execute("https://www.netroby.com/api");
+        /**
         TextView tv2 = (TextView) findViewById(R.id.textView2);
         tv2.setText("Hello world, I am glad to see you here");
         TextView tv = (TextView) findViewById(R.id.textView);
